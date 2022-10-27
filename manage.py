becdafaf093b62b678fc99c71b3b2be8b0c0d693
DavidCh <DavidChincharashvili@gmail.com>
2022-10-27 13:01:24 +0400

    feat: added a method - get_source_type

--- manage.py
+++ manage.py
@@ -20,18 +20,50 @@ class Sypter:
         - url
         - local_file HTML
         """
-        # Check if string is url regex
-
 
         geckodriver_autoinstaller.install()
         # Check if the current version of geckodriver exists
         # and if it doesn't exist, download it automatically,
         # then add geckodriver to path
+
         firefox_options = Options()
         # firefox_options.add_argument("--headless")
         self._driver = webdriver.Firefox(options=firefox_options)
         self._driver.implicitly_wait(10)
+
+        self.source_type = self._get_source_type(source)
+        if self.source_type == "url":
+            self._driver.get(source)
+        elif self.source_type == "file":
+            self._driver.get(f"file://{source}")
+        elif self.source_type == "html":
+            self._driver.get(f"data:text/html;charset=utf-8,{source}")
+
         self._driver.get(source)
+
+    # შეიძლება ამის ცალკე გატანაც
+    def _get_source_type(self, source):
+        import re
+        url_regex = re.compile(
+            r'^(?:http|ftp)s?://'  # http:// or https://
+            # domain...
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+            r'localhost|'  # localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        if re.match(url_regex, source):
+            return 'url'
+        else:
+            # Check if string is local file
+            import os
+            if os.path.isfile(source):
+                return "file"
+            # Check if string is HTML
+            elif source.startswith('<') and source.endswith('>'):
+                return "html"
+            else:
+                raise ValueError("Invalid source")
 
     def if_exists_by_id(self, element_id: str) -> bool:
         """
@@ -77,9 +109,6 @@ class Sypter:
         elif number:
             return class_count
 
-    def count_classes(self, class_name, number=1):
-        pass
-
     @staticmethod
     def has_attribute(self, selenium_obj, attribute_name: str | tuple) -> bool:
         pass
@@ -108,23 +137,41 @@ class Sypter:
 
 
 if __name__ == "__main__":
-    sypter = Sypter('https://www.w3schools.com/html/tryit.asp?filename=tryhtml_id_css')
-    print(sypter.if_exists_by_id("iframe"))
+    # sypter = Sypter('https://www.w3schools.com/html/tryit.asp?filename=tryhtml_id_css')
+    # print(sypter.if_exists_by_id("iframe"))
+    #
+    # sypter = Sypter('https://davidunilab.github.io/davidunilab.front-lesson12/')
+    # # TRUTH
+    # print(sypter.if_exists_by_class("paragraph", number=2))
+    # print(sypter.if_exists_by_class("paragraph", min_num=2))
+    # print(sypter.if_exists_by_class("paragraph", max_num=2))
+    # print(sypter.if_exists_by_class("paragraph", min_num=2, max_num=2))
+    # print(sypter.if_exists_by_class("paragraph", min_num=1, max_num=2))
+    # print(sypter.if_exists_by_class("paragraph", min_num=0, max_num=2))
+    # print(sypter.if_exists_by_class("paragraph", number=2, min_num=1, max_num=2))
+    # print(sypter.if_exists_by_class("paragraph", number=2, min_num=2, max_num=2))
+    #
+    # # FALSE
+    # print(sypter.if_exists_by_class("paragraph", number=1))
 
-    sypter = Sypter('https://davidunilab.github.io/davidunilab.front-lesson12/')
-    # TRUTH
-    print(sypter.if_exists_by_class("paragraph", number=2))
-    print(sypter.if_exists_by_class("paragraph", min_num=2))
-    print(sypter.if_exists_by_class("paragraph", max_num=2))
-    print(sypter.if_exists_by_class("paragraph", min_num=2, max_num=2))
-    print(sypter.if_exists_by_class("paragraph", min_num=1, max_num=2))
-    print(sypter.if_exists_by_class("paragraph", min_num=0, max_num=2))
-    print(sypter.if_exists_by_class("paragraph", number=2, min_num=1, max_num=2))
-    print(sypter.if_exists_by_class("paragraph", number=2, min_num=2, max_num=2))
+    html = """
+        data:text/html;charset=utf-8,
+        <div class="post-signature owner flex--item">
+            <div class="user-info user-hover">
+            <div class="user-action-time">
+                asked <span title="2021-12-09 12:02:00Z" class="relativetime">38 mins ago</span>
+            </div>
+            <div class="user-gravatar32">
+                <a href="/users/4094231/umair-ayub"><div class="gravatar-wrapper-32"><img src="https://www.gravatar.com/avatar/da5d12e4b0c319022432a23fc5344831?s=64&amp;d=identicon&amp;r=PG&amp;f=1" alt="" width="32" height="32" class="bar-sm"></div></a>
+            </div>
+            <div class="user-details" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+                <a href="/users/4094231/umair-ayub">Umair Ayub</a><span class="d-none" itemprop="name">Umair Ayub</span>
+                <div class="-flair">
+                    <span class="reputation-score" title="reputation score 14,591" dir="ltr">14.6k</span><span title="12 gold badges" aria-hidden="true"><span class="badge1"></span><span class="badgecount">12</span></span><span class="v-visible-sr">12 gold badges</span><span title="61 silver badges" aria-hidden="true"><span class="badge2"></span><span class="badgecount">61</span></span><span class="v-visible-sr">61 silver badges</span><span title="131 bronze badges" aria-hidden="true"><span class="badge3"></span><span class="badgecount">131</span></span><span class="v-visible-sr">131 bronze badges</span>
+                </div>
+            </div>
+        </div>
+        </div>
+    """
 
-    # FALSE
-    print(sypter.if_exists_by_class("paragraph", number=1))
-
-
-
-
+    Sypter(html)
