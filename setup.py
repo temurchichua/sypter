5a811f9f3194f2e5cc04ac9edb009b84fc616562
DavidCh <DavidChincharashvili@gmail.com>
2022-11-04 11:05:50 +0400

    feat: structure change, added setup, license

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="Sypter",
+    version="0.0.1",
+    author="David Chincharashvili",
+    description="Package for testing purposes",
+    packages=["sypter"],
+    python_requires=">=3.6",
+)
